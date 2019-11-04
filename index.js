8b7f1d85c9979410f6a7d528b15740ba1b85bf4b
andreybozhkov <bozhkov.andrey@gmail.com>
2019-11-04 17:26:43 +0200

    Added sheet to JSON basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const XLSX = require('xlsx');
+
+let readOptions = {
+    type: 'string',
+    cellFormula: false,
+    cellText: false,
+    cellDates: true
+};
+
+let workbook = XLSX.readFile('./data/initial/import-projects-0.xls', readOptions);
+let workbookSheetImportClean = workbook.Sheets['Import_clean'];
+let sheetJSON = XLSX.utils.sheet_to_json(workbookSheetImportClean);
+console.log(sheetJSON);
