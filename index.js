4c2f42587727aa0aacd615e89b1d3f0aae0bd358
andreybozhkov <bozhkov.andrey@gmail.com>
2019-11-05 13:41:39 +0200

    Update iteration function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let readOptions = {
     type: 'string',
     cellFormula: false,
     cellText: false,
-    cellDates: true
+    cellDates: false
 };
 
 let workbookImport = XLSX.readFile('./data/initial/import-projects-0.xls', readOptions);
@@ -13,30 +13,35 @@ let workbookExport = XLSX.readFile('./data/initial/export-projects-0.xls', readO
 let exportJSON = XLSX.utils.sheet_to_json(workbookExport.Sheets['Export_clean']);
 let reportJSON = [];
 
+function addNewRoundTrip (importProject, exportProject) {
+    let newRoundTrip = {...importProject};
+    newRoundTrip['Export ID'] = exportProject.ID;
+    newRoundTrip['Export Trailer'] = exportProject.Trailer;
+    newRoundTrip['Export Project Reporting Date'] = exportProject['Project Reporting Date'];
+    newRoundTrip['Export Start Date'] = exportProject['Start Date'];
+    newRoundTrip['Export End Date'] = exportProject['End Date'];
+    newRoundTrip['Export Traffic Line Group'] = exportProject['Traffic Line Group'];
+    newRoundTrip['Export Traffic Line'] = exportProject['Traffic Line'];
+    newRoundTrip['Export Estimated Net Profit'] = exportProject['Estimated Net Profit'];
+    newRoundTrip['Export Net Profit'] = exportProject['Net Profit'];
+    newRoundTrip['Export Customer Companies from Shipments'] = exportProject['Customer Companies from Shipments'];
+    reportJSON.push(newRoundTrip);
+}
+
 for (importProject of importJSON) {
     for (exportProject of exportJSON) {
         if (exportProject.Trailer === importProject.Trailer && exportProject['Start Date'] > importProject['Start Date']) {
-            let newRoundTrip = {...importProject};
-            newRoundTrip['Export ID'] = exportProject.ID;
-            newRoundTrip['Export Trailer'] = exportProject.Trailer;
-            newRoundTrip['Export Project Reporting Date'] = exportProject['Project Reporting Date'];
-            newRoundTrip['Export Start Date'] = exportProject['Start Date'];
-            newRoundTrip['Export End Date'] = exportProject['End Date'];
-            newRoundTrip['Export Traffic Line Group'] = exportProject['Traffic Line Group'];
-            newRoundTrip['Export Traffic Line'] = exportProject['Traffic Line'];
-            newRoundTrip['Export Estimated Net Profit'] = exportProject['Estimated Net Profit'];
-            newRoundTrip['Export Net Profit'] = exportProject['Net Profit'];
-            newRoundTrip['Export Customer Companies from Shipments'] = exportProject['Customer Companies from Shipments'];
-            reportJSON.push(newRoundTrip);
+            addNewRoundTrip(importProject, exportProject);
+            break;
         }
     }
 }
 
 let reportWorksheet = XLSX.utils.json_to_sheet(reportJSON, {
-    cellDates: true
+    cellDates: false
 });
 
 let newWorkbook = XLSX.utils.book_new();
-XLSX.utils.book_append_sheet(newWorkbook, reportWorksheet, 'report');
+XLSX.utils.book_append_sheet(newWorkbook, reportWorksheet, 'data');
 
 XLSX.writeFile(newWorkbook, 'report.xlsx');
