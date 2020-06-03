7f448a7111090f916be95657644e6c7b2d66eb8a
andreybozhkov <bozhkov.andrey@gmail.com>
2020-06-03 18:06:12 +0300

    Update path name extensions

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ let readOptions = {
 
 let workbookCustomerReferences = XLSX.readFile('./data/initial/Holmen Paper/2019-11 and 12/edge_protectors_input.xlsx', readOptions);
 let customerRefsJSON = XLSX.utils.sheet_to_json(workbookCustomerReferences.Sheets['edge_protectors']);
-let workbookShipments = XLSX.readFile('./data/initial/Holmen Paper/2019-11 and 12/shipments.xls', readOptions);
+let workbookShipments = XLSX.readFile('./data/initial/Holmen Paper/2019-11 and 12/shipments.xlsx', readOptions);
 let shipmentsJSON = XLSX.utils.sheet_to_json(workbookShipments.Sheets['shipments']);
-let workbookProjects = XLSX.readFile('./data/initial/Holmen Paper/2019-11 and 12/projects.xls', readOptions);
+let workbookProjects = XLSX.readFile('./data/initial/Holmen Paper/2019-11 and 12/projects.xlsx', readOptions);
 let projectsJSON = XLSX.utils.sheet_to_json(workbookProjects.Sheets['projects']);
 let reportJSON = [];
 
